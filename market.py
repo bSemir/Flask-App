9f96e36908680cf548f5320683f519364eb3f55f
Semir <sblekic1@etf.unsa.ba>
2022-11-25 16:53:50 +0100

    templates folder, navbar, 1st html page rendered

--- market.py
+++ market.py
@@ -1,14 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(
-    __name__)  # __name__ is "magic" variable that we can always call and it refers to a file we are curently working with
+app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello gracious people!</p>"
-
-
-@app.route("/about/<username>")
-def about_page(username):
-    return f'<h1>This is a about page for {username}</h1>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
